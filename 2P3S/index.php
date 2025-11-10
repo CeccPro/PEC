26da3784892e641f43f004be5443614c07933ac7
CeccPro <pokis1214@gmail.com>
2025-11-10 09:25:05 -0600

    A

---
+++ 2P3S/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>EcoBlog</title>
+        <link rel="stylesheet" href="./css/bootstrap.min.css">  
+        <link rel="stylesheet" href="./css/styles.css">
+    </head>
+    <body class="eb-bg-dark">
+        <div class="container text-center mb-4">
+            <!-- Bold text and custom color -->
+            <h1 class="title-light-green mt-4 fw-bold">EcoBlog</h1>
+        </div>
+        <div class="container text-center text-white p-5 green-container w-50">
+            <h1>Iniciar Sesión</h1>
+            <div class="container d-flex">
+
+            </div>
+        </div>
+    </body>
+</html>
